e9b067f8610cbf47bc4987a51b4d43f5f00885cb
bishma <ariabishma848@gmail.com>
2019-07-03 22:24:50 +0700

    tambah header

--- components/head.php
+++ components/head.php
@@ -5,3 +5,11 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+
+<!-- script header disini -->
+<div class="navbar navbar-expand navbar-light bg-light">
+	<div class="container">
+		<a class="navbar-brand ">Search Engine </a>
+		<a class="navbar-brand ml-auto"><small class="bg-warning">(<strong>header sementara </strong>) </a>
+	</div>
+</div>
---
+++ detail.php
@@ -0,0 +1,11 @@
+<?php include 'components/head.php' ?>
+
+<!-- Tulis Konten Disini ! -->
+
+<h1>Hello World</h1>
+
+<!-- akhir konten -->
+
+
+<?php include 'components/foot.php' ?>
+
