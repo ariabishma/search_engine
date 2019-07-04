e90ddb7ad1e7109974ffa90859fc89751d6352c3
bishma <ariabishma848@gmail.com>
2019-07-04 19:23:22 +0700

    ganti font

--- components/head.php
+++ components/head.php
@@ -3,6 +3,12 @@
 <head>
 	<title>Google nya Talaga...</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Titillium+Web&display=swap');
+		body{
+			font-family: 'Titillium Web', sans-serif;
+		}
+	</style> 
 </head>
 <body>
 
