ed58d5852be0156fe2fcd13e234c7caf0131c40d
ravialdo <ravialdo@yahoo.com>
2019-07-04 20:23:24 +0700

    tambah folder assets

--- components/head.php
+++ components/head.php
@@ -1,23 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Google nya Talaga...</title>
+	<title>Google Talaga Version - Search</title>
+	<!-- Required meta tags --> 
+	 <meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+      <!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	 <!-- Style CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+
 </head>
 <body>
-
-<!-- script header disini -->
-<nav class="navbar navbar-light bg-light">
-	<div class="container">
-	<a class="navbar-brand">wahengine</a>
-
-    <div class="input-group mr-auto" style="width: 40%;">
-      
-      <input type="text" class="form-control" placeholder="Cari..." aria-label="Username" aria-describedby="basic-addon1">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">#</span>
-      </div>
-    </div>
-
-  </div>
-</nav>
