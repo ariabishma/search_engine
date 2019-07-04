528b26d71884b195f6b607687e8dda2cb83b6f4d
ravialdo <ravialdo@yahoo.com>
2019-07-05 05:29:37 +0700

    lanjut bambang :v

--- components/head.php
+++ components/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Google Talaga Version - Search</title>
+	<title>WahEngine</title>
 	<!-- Required meta tags --> 
 	 <meta charset="utf-8">
 	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
