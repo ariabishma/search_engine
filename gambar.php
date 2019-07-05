5460ec7954c7115b971bc6a9c6d981996a0fc5fc
ravialdo <ravialdo@yahoo.com>
2019-07-05 13:06:08 +0700

    view gambar.php

--- gambar.php
+++ gambar.php
@@ -1,5 +1,25 @@
 <?php include 'components/head.php'; ?>
     <!-- konten tulis disini -->
 
+		
+<div class="row">
+	<div class="col-md-6 ml-130 mx-20">
+	
+			<form action="" method="">
+		
+				<div  class="form-group">
+			
+				<input type="text" class="form-control mt-3" placeholder="Cari..">
+				
+			</div>
+			
+			<button type="submit" class="btn btn-success float-right mt-54">Cari</button>
+
+		</form>
+	
+	</div>
+</div>
+
+
 	<!-- akhir konten disini -->
 <?php include 'components/foot.php'; ?>
