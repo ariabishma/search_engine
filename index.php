73f78910fb89bf87553f180803f070ed2e14a01e
ravialdo <ravialdo@yahoo.com>
2019-07-05 08:08:25 +0700

    (+) view register.php

--- index.php
+++ index.php
@@ -26,7 +26,13 @@
 		</form>
 		
 		<div class="text-center text-14">Google talaga ditawarkan di : <a href="https://en.m.wikipedia.org/wiki/Indonesia"> Indonesian </a> </div>
-
+    	
+		<div class="text-center mt-3">
+			<a href="login.php">Sign In</a> -
+			<span>
+				<a href="register.php">Register</a>
+			</span>
+		</div>
 	</div>
 </div>
 
--- register.php
+++ register.php
@@ -7,7 +7,8 @@
 		<div class="col-md-7">
 			<div class="card mb-5">
 				<div class="card-header bg-transparent ">
-					<h4 class="m-0">Sign In</h4>
+					<h4 class="m-0">Register</h4>
+					
 				</div>
 				<div class="card-body">
 					<form>
@@ -16,15 +17,20 @@
 								 <input type="text" class="form-control" placeholder="Username..">
 						 </div>
 						<div class="form-group">
-							 <label>Password</label>
-								 <input type="password" class="form-control" placeholder="Password..">
+							 <label>Email</label>
+								 <input type="email" class="form-control" placeholder="Email..">
 						 </div>
-						<button class="btn btn-success">Sign in</button>
-						
+						<div class="form-group">
+							 <label>Password</label>
+								 <input type="text" class="form-control" placeholder="Password..">
+						 </div>
+						<div class="form-group">
+							 <label>Retype Password</label>
+								 <input type="text" class="form-control" placeholder="retype Password..">
+						 </div>
+						<button class="btn btn-success">Register</button>
 					</form>
-					
-					<a href="" class="float-right">I forgot Password?</a>
-					
+					<a href="" class="float-right">I have a account?</a>
 				</div>
 			</div>
 		</div>
