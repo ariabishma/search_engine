82345734c02d3612aa481367a6c721490c5684b6
bishma <ariabishma848@gmail.com>
2019-07-05 18:37:54 +0700

    remove search bar

--- gambar.php
+++ gambar.php
@@ -2,23 +2,7 @@
     <!-- konten tulis disini -->
 
 		
-<div class="row">
-	<div class="col-md-6 ml-130 mx-20">
-	
-			<form action="" method="">
-		
-				<div  class="form-group">
-			
-				<input type="text" class="form-control mt-3" placeholder="Cari..">
-				
-			</div>
-			
-			<button type="submit" class="btn btn-success float-right mt-54">Cari</button>
 
-		</form>
-	
-	</div>
-</div>
 
 <div class="container mb-4">
 
