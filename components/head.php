79b7e225b95eb323c186006519bc50dcaf3e4d0a
ravialdo <ravialdo@yahoo.com>
2019-07-08 09:53:41 +0700

    update script

--- components/head.php
+++ components/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WahEngine</title>
+	<title>SoftTech Engine</title>
 	<!-- Required meta tags --> 
 	 <meta charset="utf-8">
 	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
@@ -22,23 +22,35 @@
 </head>
 <body>
 
-<!-- script header disini -->
-<nav class="navbar navbar-light bg-light">
-	<div class="container">
-	<a class="navbar-brand">wahengine</a>
-
-    <div class="input-group mr-auto" style="width: 40%;">
-      
-      <input type="text" class="form-control" placeholder="Cari..." aria-label="Username" aria-describedby="basic-addon1">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">#</span>
-      </div>
-    </div>
-
-  </div>
-</nav>
-
-
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-white">
+  <div class="container">
+ 	<a class="navbar-brand font-weight-bold" href="#"> <i class="fa fa-shopping-cart text-primary font-italic font-weight-bold"></i>  SoftTech Engine</a>
+ 		<button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		 	<i class="fa fa-align-justify text-primary"></i>
+		 </button>
+ 		 <div class="collapse navbar-collapse" id="navbarNav">
+			 <ul class="navbar-nav">
+ 				<li class="nav-item active">
+ 					<a class="nav-link" href="/"><i class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+				 </li> <li class="nav-item"> <a class="nav-link" href="login.php"><i class="fa fa-sign-in"></i> Sign In</a> 
+				 </li> <li class="nav-item"> <a class="nav-link" href="register.php"><i class="fa fa-clipboard"></i> Register</a>
+ 				 </li> <li class="nav-item"> <a class="nav-link" href="#">
+				<form action="pencarian.php" method="">
+						<div class="input-group mb-3">
+							 <input type="text" class="form-control border-0 brdr-bttm" placeholder="Cari ..">
+								 <div class="input-group-append">
+									 <button class="btn btn-transparent border-0 border-bottom brdr-bttm" type="submit"> <i class="fa fa-search"></i></button>
+								 </div>
+						 </div> 
+				</form>
+				</a>
+			 </li>
+		 </ul>
+	 </div>
+	</div>
+ </nav>
+<!-- and navbar -->
 </head>
 <body>
 
