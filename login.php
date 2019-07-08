181a9e81d51f302e9bbc87c1ec56e8f04f767c03
ravialdo <ravialdo@yahoo.com>
2019-07-08 09:59:49 +0700

    update script

--- login.php
+++ login.php
@@ -2,34 +2,57 @@
 
 <!-- Tulis Konten Disini ! -->
 
-<div class="container mt-5">
+<div class="container-fluid jumbotron">
+
+<div class="row justify-content-center">
+	<div class="col-md-4 text-center">
+			<div class="font-weight-bold h1">
+						Masuk
+						<div class="text-muted h2">
+								
+						</div>
+				</div>
+	</div>
+</div>
+
+</div>
+
+<div class="container">
 	<div class="row justify-content-center">
-		<div class="col-md-7">
-			<div class="card mb-5">
-				<div class="card-header bg-transparent ">
-					<h4 class="m-0">Sign In</h4>
-				</div>
-				<div class="card-body">
-					<form>
-						<div class="form-group">
-							 <label>Username</label>
-								 <input type="text" class="form-control" placeholder="Username..">
-						 </div>
-						<div class="form-group">
-							 <label>Password</label>
-								 <input type="password" class="form-control" placeholder="Password..">
-						 </div>
-						<button class="btn btn-success rounded-20">Sign in</button>
-						
-					</form>
-					
-					<a href="" class="float-right">I forgot Password?</a>
-					
-				</div>
-			</div>
+		<div class="col-md-11 bg-primary rounded-top text-center text-white shadow">
+			<h4 class="font-weight-bold mt-5">Akun anda?</h4>
+			<p style="font-size:20px;margin-bottom:30px;">
+				Akun anda akan selalu kami amankan demi kenyamanan anda <br> dan hal lainnya yang bersifat privasi
+			</p>
+			<a href="#" class="btn border-white btn-sm text-white btn-circle" role="button"><i class="fa fa-clipboard"></i> Buat Akun</a>
 		</div>
 	</div>
 </div>
+
+<div class="container mb-5">
+	<div class="row justify-content-center">
+		<div class="col-md-11 bg-white rounded-bottom text-center text-dark shadow">
+				<h2 class="font-weight-bold mt-4">Silahkan Masuk</h2>
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-5 mb-4">
+							<form>
+								<div class="form-group">
+						 			<input type="text" class="form-control border-0 brdr-bttm" placeholder="Username">
+							    </div>
+								<div class="form-group">
+									 <input type="text" class="form-control border-0 brdr-bttm" placeholder="Password">
+								</div>
+								<button class="btn btn-primary rounded-20"><i class="fa fa-sign-in"></i> Masuk</button>
+							</form>
+							<a class="text-muted">Lupa Password?</a>
+						</div>
+					</div>	
+				</div>
+		</div>
+	</div>
+</div>
+
 
 <!-- akhir konten -->
 
