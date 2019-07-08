f3f5c6ba1ce5b93057422291d5c8c68b1f8243a5
ravialdo <ravialdo@yahoo.com>
2019-07-08 09:52:30 +0700

    update script

--- components/foot.php
+++ components/foot.php
@@ -1,3 +1,32 @@
+<div class="container-fluid bg-dark pb-4 <?php echo $footer ?>">
+
+<div class="row justify-content-center">
+ 	<div class="col-md-4">
+	 	<div class="title-foot text-white mt-2">Authors</div>
+			 <div class="text-foot text-muted">
+				<i class="fa fa-user"></i> Aria Bishma (Fullstack Developer Web) <br>
+				<i class="fa fa-user"></i> Ravialdo (Fullstack Developer Web) <br>
+			</div>
+			<div class="title-foot text-white mt-2">Unduh Source Code softTect Engine</div>
+			 <div class="text-foot text-muted">
+					<a href="#" class="btn btn-primary btn-lg" role="button" style="border-radius:50px;"><i class="fa fa-download text-white"></i> Unduh disini</a>
+			</div>
+		</div>
+	<div class="col-md-4">
+		<div class="title-foot text-white mt-2">Kontak</div>
+			 <div class="text-foot">
+				<a class="text-success" href="https://api.whatsapp.com/send?phone=6288802256843&text=Hello SoftTech Engine!"> <i class="fa fa-whatsapp fa-1x"></i>  Aria Bishma </a> <br>
+				<a class="text-success" href="https://api.whatsapp.com/send?phone=6282215037870&text=Hello SoftTech Engine!"> <i class="fa fa-whatsapp fa-1x"></i>  Ravialdo </a> <br>
+			</div>
+					<div class="title-foot text-white mt-2">Tautan Cepat</div>
+						  <a href="index.php"> <i class="fa fa-home"></i> Home</a> <br>
+						  <a href="login.php"> <i class="fa fa-sign-in"></i> Sign In</a> <br>
+						  <a href="register.php"> <i class="fa fa-clipboard"></i> Register</a> <br>
+	</div>
+</div>
+		<hr class="border-primary">
+		<div class="text-white mt-4 mb-2 text-center">Copyright 2019 <i class="fa fa-heart fa-spin text-danger"> </i> Search Over The Internet Technology </div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
