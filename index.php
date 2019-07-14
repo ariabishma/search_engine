1849a695b5488980e1962667497a5f35e99d987a
ravialdo <ravialdo@yahoo.com>
2019-07-14 19:20:20 +0700

    update script

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 <?php include 'components/head.php' ?>
 
+<style type="text/css">
+	.bg-image {
+		background-image:url('assets/img/bg-image.jpg');
+		background-size:cover;
+		-webkit-filter:brightness(50%);
+		width: 100%;
+		height: 200px;
+		position: absolute;
+	}
+
+	.input-radius {
+		border-radius: 50px;
+		border-right: 0;
+		border-bottom-right-radius: 0;
+		border-top-right-radius: 0;
+	}
+
+	.btn-radius {
+		border-radius: 50px;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+		border-left: 0;
+	}
+
+	.form-index {
+		margin-top: 30px;
+		margin-bottom: 30px;
+	}
+</style>
+
 <!-- Tulis Konten Disini ! -->
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-12  jumbotron ">
+		<div class="bg-image">
+		</div>
+		<div class="col-md-12">
 			<div class="container">
 				<form action="pencarian.php">
-					<span>Contoh : Laptop</span>
-					<input name="req" type="text" class="form-control py-4" placeholder="Jelajahi Produk Dari Seluruh situs internet...">
-					<a href="#" class="btn btn-primary float-right mt-3 ml-pas btn-lg shadow text-white" role="button"><i class="fa fa-shopping-bag"></i> JELAJAHI SEKARANG</a>
+					  <div class="input-group form-index">
+					      <input type="text" name="req" class="form-control input-radius" placeholder="Jelajahi Produk Dari Seluruh situs internet..." aria-label="Search for...">
+					      <span class="input-group-btn">
+					        <button class="btn btn-primary btn-radius" type="submit">
+					        	<i class="fa fa-search"></i>
+					        </button>
+					      </span>
+					    </div>
+					<span class="ml-2 text-white">Contoh : Laptop</span>
 				</form>
-			</div>
-		    
+			</div>	    
 		</div>
 	</div>
 </div>
@@ -54,7 +91,7 @@
 							<div class="text-muted"> <i class="fa fa-tags"></i> Accessories</div>
 							<div class="text-muted"> <i class="fa fa-user"></i> Bukalapak</div>
 
-							<a target="_blank" href="<?= $r['url'] ?>"><button style="position:absolute;bottom:10px; left:5%;width:90%;" class="btn btn-primary mt-3 btn-block btn-buy">
+							<a target="_blank" href="<?= $r['url'] ?>"><button style="position:absolute;border-radius:50px;bottom:10px; left:5%;width:90%;" class="btn btn-primary mt-3 btn-block btn-buy">
 								<i class="fa fa-cart-arrow-down">  Rp.<?= number_format($r['harga']) ?> </i>
 							</button></a>
 								
